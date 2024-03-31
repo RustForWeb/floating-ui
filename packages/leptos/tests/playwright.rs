9ad14042210a18eaa2a92e6b45b12247fe6a7fa2
Danielle Huisman <danielle@nixy.software>
2024-03-31 18:34:02 +0200

    Move visual tests folder

--- packages/leptos/tests/playwright.rs
+++ packages/leptos/tests/playwright.rs
@@ -4,10 +4,11 @@ use std::{env, fs, path::Path, process::Command};
 pub fn playwright() {
     let repository_url = "https://github.com/floating-ui/floating-ui";
     let repository_path = Path::new(env!("CARGO_TARGET_TMPDIR")).join("floating-ui");
+    let repository_dom_path = repository_path.join("packages/dom");
+    let repository_playwright_config_path =
+        repository_dom_path.join("packages/dom/playwright.config.ts");
 
-    // if repository_path.exists() {
-    //     fs::remove_dir_all(repository_path.clone()).expect("Removing repository failed.");
-    // }
+    let visual_test_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("test/visual");
 
     if !repository_path.exists() {
         let status = Command::new("git")
@@ -34,9 +35,8 @@ pub fn playwright() {
         assert!(status.success(), "Git pull failed.");
     }
 
-    let config_path = repository_path.join("packages/dom/playwright.config.ts");
-    let mut config_content =
-        fs::read_to_string(config_path.clone()).expect("Reading Playwright config file failed.");
+    let mut config_content = fs::read_to_string(repository_playwright_config_path.clone())
+        .expect("Reading Playwright config file failed.");
 
     config_content = config_content
         .replace("retries: 3,", "retries: 0,")
@@ -44,14 +44,12 @@ pub fn playwright() {
             "command: 'pnpm run dev',",
             &format!(
                 "command: 'trunk serve --port 1234',\n    cwd: '{}',\n    stdout: 'pipe',",
-                Path::new(env!("CARGO_MANIFEST_DIR"))
-                    .join("test/visual")
-                    .to_str()
-                    .expect("Path should be valid.")
+                visual_test_path.to_str().expect("Path should be valid.")
             ),
         );
 
-    fs::write(config_path, config_content).expect("Writing Playwright config file failed.");
+    fs::write(repository_playwright_config_path, config_content)
+        .expect("Writing Playwright config file failed.");
 
     let status = Command::new("pnpm")
         .arg("install")
@@ -59,8 +57,6 @@ pub fn playwright() {
         .status()
         .expect("pnpm install failed.");
     assert!(status.success(), "pnpm install failed");
-
-    let dom_path = repository_path.clone().join("packages/dom");
 
     let status = Command::new("pnpm")
         .arg("run")
@@ -78,7 +74,7 @@ pub fn playwright() {
         let status = Command::new("npx")
             .arg("playwright")
             .arg("install")
-            .current_dir(dom_path.clone())
+            .current_dir(repository_dom_path.clone())
             .status()
             .expect("Playwright install failed.");
         assert!(status.success(), "Playwright install failed.");
@@ -87,7 +83,7 @@ pub fn playwright() {
     let status = Command::new("pnpm")
         .arg("run")
         .arg("playwright")
-        .current_dir(dom_path.clone())
+        .current_dir(repository_dom_path.clone())
         .status()
         .expect("Playwright tests failed.");
     assert!(status.success(), "Playwright tests failed.");
