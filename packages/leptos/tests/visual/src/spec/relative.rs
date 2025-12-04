08a85711c7b12e7978a8dde34a3dd1fb772e59e4
Daniëlle Huisman <danielle@huisman.me>
2025-12-04 09:59:27 +0100

    fix: wrap Leptos text in quotes

--- packages/leptos/tests/visual/src/spec/relative.rs
+++ packages/leptos/tests/visual/src/spec/relative.rs
@@ -80,8 +80,8 @@ pub fn Relative() -> impl IntoView {
     view! {
         <h1>Relative</h1>
         <p>
-            The floating element should be positioned correctly on the bottom when a
-            certain parent node has <code>position: relative</code> applied.
+            "The floating element should be positioned correctly on the bottom when a
+            certain parent node has "<code>"position: relative"</code>" applied."
         </p>
         <div
             class="container"
